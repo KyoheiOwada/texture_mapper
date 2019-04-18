c97072d0d23742166f502cfcdd38431f204ec20c
Kyohei Owada <44285607+KyoheiOwada@users.noreply.github.com>
2019-04-18 18:21:32 +0900

    Update uvmapper.py

--- uvmapper.py
+++ uvmapper.py
@@ -23,3 +23,30 @@ def uvMapperCylinder(obj):
     bpy.data.materials[matname].active_texture = texUV
     bpy.data.materials[matname].texture_slots[0].texture_coords = "OBJECT"
     bpy.data.materials[matname].texture_slots[0].mapping = "TUBE"
+    
+    
+ def delete_old_stuff():
+
+    # escape edit mode
+    if bpy.ops.object.mode_set.poll():
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+    # delete all mesh objects
+    bpy.ops.object.select_by_type(type='MESH')
+    bpy.ops.object.delete()
+
+    # delete all materials
+    for i in bpy.data.materials.values():
+        bpy.data.materials.remove(i)
+
+    # delete all textures
+    for i in bpy.data.textures.values():
+        bpy.data.textures.remove(i)
+
+    # delete all images 
+    for i in bpy.data.images.values():
+        # delete image path, this is only possible without a user
+        i.user_clear()
+        # delete all, except »Render Result«
+        if i.name != "Render Result":
+            bpy.data.images.remove(i)
