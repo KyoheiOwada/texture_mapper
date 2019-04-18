e228c053d6fb8389a58de35504c675392a05e990
Kyohei Owada <44285607+KyoheiOwada@users.noreply.github.com>
2019-04-18 13:17:09 +0900

    Add files via upload

---
+++ uvmapper.py
@@ -0,0 +1,25 @@
+import bpy
+import os
+
+
+def uvMapperCylinder(obj):
+    
+    matname = "cylindermat"
+    texname = "cylindertex"
+    # new material
+    if not matname in bpy.data.materials:
+        material = bpy.data.materials.new(matname)
+        obj.data.materials.append(material)
+        
+    # new texture    
+    texUV = bpy.data.textures.new(texname,type = "IMAGE")
+    # Write the path of folder which store a image
+    image_path = os.getenv("HOMEDRIVE") + os.getenv("HOMEPATH") + "\\Pictures\\texture_image\\kukki.png"
+    image = bpy.data.images.load(image_path)
+    texUV.image = image
+    
+    #connect texture with material
+    bpy.data.materials[matname].texture_slots.add()
+    bpy.data.materials[matname].active_texture = texUV
+    bpy.data.materials[matname].texture_slots[0].texture_coords = "OBJECT"
+    bpy.data.materials[matname].texture_slots[0].mapping = "TUBE"
